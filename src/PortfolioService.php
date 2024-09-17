5455ecbcf1928df8dcd06286d025c4fec2da8c40
MartkCz <martk494@gmail.com>
2024-09-17 19:19:45 +0200

    feat(core): add new endpoint

--- src/PortfolioService.php
+++ src/PortfolioService.php
@@ -2,6 +2,8 @@
 
 namespace Api\Portfolio;
 
+use Api\Core\Exception\InvalidRequestException;
+use Api\Core\Exception\UnrecoverableRequestException;
 use Api\Core\RequestType;
 use Api\Core\Service;
 use Api\Core\ServiceRequest;
@@ -19,6 +21,7 @@ final class PortfolioService extends Service
 	public const TimeSeriesLink = '/portfolio/timeseries-value';
 	public const ValueLink = '/portfolio/value';
 	public const PerformanceLink = '/portfolio/performance';
+	public const ImportLink = '/portfolio/import';
 
 	/**
 	 * @param Transaction[] $transactions
@@ -46,6 +49,31 @@ final class PortfolioService extends Service
 		return $this->requestJson(RequestType::Post, $transactions, self::ValueLink, [
 			'currency' => $currency,
 		]);
+	}
+
+	/**
+	 * @param mixed[] $transactions
+	 * @return array{ symbol: string, shares: float, investment: float, date: string }[]
+	 * @throws InvalidRequestException
+	 */
+	public function import(array $transactions, bool $validate = false, bool $splits = false): array
+	{
+		$response = $this->requestJson(RequestType::Post, $transactions, self::ImportLink, [
+			'splits' => $splits ? '1' : '0',
+			'validate' => $validate ? '1' : '0',
+		])->request();
+
+		$code = $response->getStatusCode();
+
+		if ($code === 200) {
+			return $response->toArray();
+		}
+
+		if ($code === 400) {
+			throw new InvalidRequestException($response);
+		}
+
+		throw new UnrecoverableRequestException($response);
 	}
 
 	/**
